9089bab6d47775f6971faee08033ba24e73a4219
paragoner1 <shears_mete8f@icloud.com>
2025-04-05 19:32:01 -0500

    finished exercises 1.1 thru 2.10

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,24 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+
+    // Define a function named `factorial` that, given a non-negative integer `n`,
+// returns `n!`, the factorial of `n`.
+//
+// The factorial of `n` is defined as the product of all positive integers up to `n`.
+// For example, `5!` (read "five factorial") is `5 * 4 * 3 * 2 * 1`, which is `120`.
+// `0!` is defined to be `1`.
+//
+// We expect `factorial(0)` to return `1`, `factorial(1)` to return `1`,
+// `factorial(2)` to return `2`, and so on.
+
+let mut result = 1;
+let mut i = 1;
+while i <= n {
+    result *= i;// result = result * i
+    i += 1; // i = i + 1
+}
+result // return result so that the function returns the value to the caller (the test)
 }
 
 #[cfg(test)]
